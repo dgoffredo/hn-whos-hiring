ce96a57d154f0b163a3cc3c83e268a33bf09f9e9
David Goffredo <dmgoffredo@gmail.com>
2024-09-03 23:40:55 -0400

    readme and license

--- stats.sql
+++ stats.sql
@@ -1,12 +1,12 @@
 select
   month_iso as "month"
 , count(*) as "jobs"
-, round(100.0*sum(ai_mentions > 0)/count(*), 1) as "ai%"
-, round(100.0*sum(remote_mentions > 0)/count(*), 1) as "remote%"
-, round(100.0*sum(onsite_mentions > 0)/count(*), 1) as "onsite%"
-, round(100.0*sum(hybrid_mentions > 0)/count(*), 1) as "hybrid%"
-, round(100.0*sum(ny_mentions > 0)/count(*), 1) as "ny%"
-, round(100.0*sum(fulltime_mentions > 0)/count(*), 1) as "fulltime%"
+, cast(round(100.0*sum(ai_mentions > 0)/count(*), 0) as integer) as "ai%"
+, cast(round(100.0*sum(remote_mentions > 0)/count(*), 0) as integer) as "remote%"
+, cast(round(100.0*sum(onsite_mentions > 0)/count(*), 0) as integer) as "onsite%"
+, cast(round(100.0*sum(hybrid_mentions > 0)/count(*), 0) as integer) as "hybrid%"
+, cast(round(100.0*sum(ny_mentions > 0)/count(*), 0) as integer) as "ny%"
+, cast(round(100.0*sum(fulltime_mentions > 0)/count(*), 0) as integer) as "fulltime%"
 from JobPost
 group by month_iso
 order by month_iso desc;
