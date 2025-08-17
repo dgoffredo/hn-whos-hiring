472bca934c3a8445ab23da7c68845f3f9cfe647c
David Goffredo <dmgoffredo@gmail.com>
2025-08-17 17:34:21 -0400

    be more careful about markup

--- month.sh
+++ month.sh
@@ -3,12 +3,12 @@
 this_month=${1:-$(date --iso | head -c 7)}
 
 python query2html.py <<END_QUERY
-select markup
+select when_unix, hn_comment_id, hn_parent_id, markup
 from JobPost
 where ai_mentions = 0
   and fulltime_mentions > 0
   and ny_mentions > 0
-  and month_iso = '$this_month'
-order by hn_comment_id;
+  and month_iso >= '$this_month'
+order by when_unix desc;
 END_QUERY
 
--- query2html.py
+++ query2html.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlite3
 import sys
 
@@ -5,9 +6,11 @@ if __name__ == '__main__':
     db = sqlite3.connect('db.sqlite')
     query = sys.stdin.read()
     print('<html><body><table border=3>')
-    for cell, in db.execute(query):
+    for when_unix, comment_id, parent_id, markup in db.execute(query):
         # Replace non-ASCII unicode characters with XML entity equivalents.
-        cell = cell.encode('ascii', 'xmlcharrefreplace').decode('ascii')
-        print(f'<tr><td>{cell}</td></tr>')
+        markup = markup.encode('ascii', 'xmlcharrefreplace').decode('ascii')
+        when_utc = datetime.datetime.utcfromtimestamp(when_unix)
+        print(f'<tr><th><a href="https://news.ycombinator.com/item?id={parent_id}#{comment_id}">{when_utc:%Y-%m-%d}</a></td></tr>')
+        print(f'<tr><td>{markup}</td></tr>')
     print('</table></body></html>')
 
